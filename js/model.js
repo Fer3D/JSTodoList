721be44008d2cd653b30b775e6bda4164dfc9329
Fernando BG <ferbg9@gmail.com>
2021-12-22 01:29:51 +0100

    made model view (doesnt work now)

---
+++ js/model.js
@@ -0,0 +1,16 @@
+export default class Model {
+    constructor() {
+        this.view = null;
+        this.todos = [];
+    }
+
+    setView(view) {
+        this.view = view;
+    }
+    getTodos() {
+        return this.todos;
+    }
+    addTodo(title, description) {
+        console.log(title, description);
+    }
+}
